5a3a03080698363bd0b4a87d3fa82578bc5a475c
felixtanhm <felixtanhm@gmail.com>
2023-10-24 18:13:32 +0800

    New | Basic structure and planning

---
+++ script.js
@@ -0,0 +1,17 @@
+function renderCard() {
+  // Generate the HTML for a single card
+  // Attach it to the card containers
+  // Add event listeners to the buttons
+}
+
+function handleRead() {
+  // Function to handle card being marked as Read
+}
+
+function handleDelete() {
+  // Function to handle card being removed from library
+}
+
+function toggleModal() {
+  // Function to handle modal display
+}
